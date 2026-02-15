b5ea2f8256114511a552b171c0a5c7f834fb71b9
Khalid <shapiev.abib@gmail.com>
2026-02-15 09:25:40 +0300

    Создал

---
+++ Промежуточная/firstCpp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+class Khalid
+{
+public:
+    Khalid()
+    {
+        age = 17;
+    }
+
+private:
+    int age;
+}
+
+int main()
+{
+    cout<<"Khalid age is"<<Khalid.age<<endl;
+}
