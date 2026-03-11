27b6950b5c922e104cbc7c0edc52fcd487f45bbb
troianova.mv@phystech.edu <troyanovamilana21@gmail.com>
2026-03-11 20:55:23 +0300

    Добавила cpp файл с тестами для CRC16

---
+++ crc.cpp
@@ -0,0 +1,10 @@
+#include "crc.h"
+
+// TODO: реализация Халида (алгоритм CRC16 по протоколу устройства)
+// Заглушка — всегда возвращает 0, тесты будут падать пока не реализовано
+uint16_t calculateCRC16(const uint8_t* data, size_t length) {
+    // Реализовать здесь
+    (void)data;
+    (void)length;
+    return 0x0000;
+}
